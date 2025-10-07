5f084eeb104f44be1dd1b94d2096de2423eea410
TaNSiRa <tansirawit2543@gmail.com>
2025-10-07 14:34:07 +0700

    .

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1727027620"
+    serviceWorkerVersion: "260021099"
   }
 });
